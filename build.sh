930de9d72174d84d905f41c6b5c0a44f0688bfad
Jack S. Hale <mail@jackhale.co.uk>
2021-07-23 10:11:41 +0200

    Remove extra whitespace.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ffcx poisson.ufl
 gcc -I/usr/local/lib/python3.8/dist-packages/ffcx/codegeneration -shared poisson.c -o libpoisson.so
-g++ -I/usr/local/lib/python3.8/dist-packages/ffcx/codegeneration -L$(pwd) main.cpp -o main -lpoisson 
+g++ -I/usr/local/lib/python3.8/dist-packages/ffcx/codegeneration -L$(pwd) main.cpp -o main -lpoisson
